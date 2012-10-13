3b6eae264e3d5e1cfef02e060d02d84ce74e25bc
Stanislav Rodionov <x.rodionov@gmail.com>
2012-10-13 18:14:02 -0400

    index.html added. text and json examples added - new and default versions.

--- JagatServer/bin/jagat.js
+++ JagatServer/bin/jagat.js
@@ -5,9 +5,40 @@ util.puts('JAGAT server starting...');
 var connect = require('connect'),
 	jagat = require('../'),
 	sharejs = require('share'),
+	hat = require('hat'),
 	server;
 
-server = connect(connect.logger());
+var server = connect(
+	connect.logger(),
+	// TODO: upgrade to connect 2.6.0+ and express for modern routing
+	connect.favicon(),
+	connect.static(__dirname + '/../frontend'),
+	connect.router(function (app) {
+		app.get('/text/?', function(req, res, next)
+		{
+			var docName;
+			docName = hat();
+			res.writeHead(303, {location: '/text.html#' + docName});
+			res.write('');
+			res.end();
+		});
+
+		app.get('/json/?', function(req, res, next)
+		{
+			var gameName;
+			gameName = hat();
+			res.writeHead(303, {location: '/json.html#' + gameName});
+			res.write('');
+			res.end();
+		});
+
+		app.get('/?', function(req, res, next)
+		{
+			res.writeHead(302, {location: '/index.html'});
+			res.end();
+		});
+	})
+);
 
 var options = require('./options') || {};
 
