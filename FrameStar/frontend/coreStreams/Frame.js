b60aa770d2f5f3d235cfcee0f82b32dad2c30416
Stanislav Rodionov <x.rodionov@gmail.com>
2013-01-06 15:32:28 -0500

    multiple tag types support added. node atom conception added. TODO: update comments.

--- FrameStar/frontend/coreStreams/Frame.js
+++ FrameStar/frontend/coreStreams/Frame.js
@@ -27,11 +27,13 @@ function Uuid()
 	/////////////////////////////////////////////////////////////////////////////////////*/
 }
 
+// TODO: update all coments below
+
 /////////////////////
-// Node Conception //
+// Atom Conception //
 /////////////////////
 
-function Node(initId, initTags, initContent)
+function Atom(initId, initTags, initContent)
 {
 	this.id = initId || new Uuid().generate();
 	this.tags = initTags|| {};
@@ -39,40 +41,259 @@ function Node(initId, initTags, initContent)
 
 	/* Description:
 	//
-	// * id - unique identifier of node object in UUID v4 format
-	// * tags - set of pairs <link nodeId>:<type_of_link nodeId>
-	// * content - link to arbitary information that this node contanes
-	///////////////////////////////////////////////////////////////////
+	// * id - unique identifier of the Node in UUID v4 format
+	// * tags - set of pairs <link nodeId>: [<type_of_link nodeId 01>, <type_of_link nodeId 01>, ...]
+	// * content - link to arbitary information that this node atom contanes
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 	// Example:
 	//
-	// node:
+	// atom:
 	// {
 	//   id: '876baa28-1cb2-44c3-add8-17baa7cd2652',
 	//   tags:
 	//   {
-	//     29cb7b44-0e43-41f8-aedf-f67deff8ff39: 'e6898f75-a09d-4e66-952e-12ef0c0c9a4b',
-	//     6fbc1e1d-e401-49d1-8021-af35c5e6be33: 'e6898f75-a09d-4e66-952e-12ef0c0c9a4b'
-	//     c67ef986-e57f-4b7d-ac98-4845543483e9: '585d5bad-8e79-45f6-8429-ebd765dec61e',
+	//     af35c5e6-0e43-41f8-aedf-f67deff8ff39: [],
+	//     29cb7b44-0e43-41f8-aedf-f67deff8ff39: ['e6898f75-a09d-4e66-952e-12ef0c0c9a4b'],
+	//     6fbc1e1d-e401-49d1-8021-af35c5e6be33: ['e6898f75-a09d-4e66-952e-12ef0c0c9a4b', '585d5bad-8e79-45f6-8429-ebd765dec61e'],
+	//     c67ef986-e57f-4b7d-ac98-4845543483e9: ['585d5bad-8e79-45f6-8429-ebd765dec61e']
 	//   },
 	//   content: 'main node'
 	// }
 	//
-	// where '29cb7b44...', '6fbc1e1d...', 'c67ef986...',
-	// 'e6898f75...', '585d5bad...' are valid nodes
-	///////////////////////////////////////////////////////////////////////////////////*/
+	// where 'af35c5e6', '29cb7b44...', '6fbc1e1d...', 'c67ef986...',
+	//       'e6898f75...', '585d5bad...' are valid nodes;
+	// [] - common type for basic tag links.
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+}
+
+/////////////////////
+// Node Conception //
+/////////////////////
+
+/* Description:
+//
+// Node = Atom01 + Atom02 + ... + Atom0N,
+// where Node.id === Atom0i.id, Node.tags === union of Atom0i.tags,
+//       Node.content === [Atom01.content, Atom02.content, ..., Atom0N.content] // TODO: think about better conception.
+*/
+
+/////////////////////////
+// Node Implementation //
+/////////////////////////
+
+function NodeControl(initAtoms)
+{
+	var nodeAtoms = initAtoms;
+
+	var getNodeContent = function()
+	{
+		var result = [];
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			result.push(nodeAtoms[i].content);
+		}
+		return result;
+	};
+
+	var setNodeContent = function(newContent)
+	{
+		for (var i = 0; i < newContent.length && i < nodeAtoms.length; ++i)
+		{
+			if (newContent[i] === undefined)
+				continue;
+			nodeAtoms[i].content = newContent[i];
+		}
+	};
+
+	var getNodeTagsAnalizer = function()
+	{
+		return new TagsAnalizer(nodeAtoms);
+	};
+
+	var getNodeTagsControl = function()
+	{
+		return new TagsControl(nodeAtoms);
+	};
+
+	this.getTagsAnalizer = getNodeTagsAnalizer;
+	this.getTagsControl = getNodeTagsControl;
+	this.getContent = getNodeContent;
+	this.setContent = setNodeContent;
+
+	/* Description: // TODO: write it.
+	//
+	// * getTagsAnalizer - ...
+	// * getTagsControl - ...
+	// * getContent - ...
+	// * setContent - ...
+	*/
+}
+
+function TagsAnalizer(initAtoms)
+{
+	var nodeAtoms = initAtoms;
+
+	var iterateTags = function(processTag)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			for (var tagClone in nodeAtoms[i].tags)
+			{
+				processTag(tagClone);
+			}
+		}
+	};
+
+	this.iterate = iterateTags;
+}
+
+function TagsControl(initAtoms)
+{
+	var nodeAtoms = initAtoms;
+
+	var clearTags = function()
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			nodeAtoms[i].tags = {};
+		}
+	};
+
+	var addTag = function(linkNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkNodeId in nodeAtoms[i].tags)
+				return;
+		}
+		// TODO: decide somehow in what atom should be tag link added
+		// STUB: add tag to 0-th atom only
+		nodeAtoms[0].tags[linkNodeId] = [];
+	};
+
+	var removeTag = function(linkNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkNodeId in nodeAtoms[i].tags)
+			{
+				delete nodeAtoms[i].tags[linkNodeId];
+			}
+		}
+	};
+
+	var getNodeTagTypesAnalizer = function(linkNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkNodeId in nodeAtoms[i].tags)
+				return new TagTypesAnalizer(nodeAtoms, linkNodeId);
+		}
+		return null;
+	};
+
+	var getNodeTagTypesControl = function(linkNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkNodeId in nodeAtoms[i].tags)
+				return new TagTypesControl(nodeAtoms, linkNodeId);
+		}
+		return null;
+	};
+
+	this.clear = clearTags;
+	this.add = addTag;
+	this.remove = removeTag;
+	this.getTagTypesAnalizer = getNodeTagTypesAnalizer;
+	this.getTagTypesControl = getNodeTagTypesControl;
+}
+
+function TagTypesAnalizer(initAtoms, initLinkNodeId)
+{
+	var nodeAtoms = initAtoms;
+	var linkId = initLinkNodeId;
+
+	var iterateTagTypes = function(processTagType)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkId in nodeAtoms[i].tags)
+			{
+				var tagTypesClone = nodeAtoms[i].tags[linkId].slice(0);
+				processTagType(tagTypesClone);
+			}
+		}
+	};
+
+	this.iterate = iterateTagTypes;
+}
+
+function TagTypesControl(initAtoms, initLinkNodeId)
+{
+	var nodeAtoms = initAtoms;
+	var linkId = initLinkNodeId;
+
+	var clearTagTypes = function()
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkId in nodeAtoms[i].tags)
+			{
+				nodeAtoms[i].tags[linkId] = [];
+			}
+		}
+	};
+
+	var addTagType = function(typeNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			// TODO: decide somehow in what atom should be tag link added
+			// TODO: check if newTagType already exists
+			// STUB: add tag type in the first atom with needed tag
+			if (linkId in nodeAtoms[i].tags)
+			{
+				nodeAtoms[i].tags[linkId].push(typeNodeId);
+				break;
+			}
+		}
+	};
+
+	var removeTagType = function(typeNodeId)
+	{
+		for (var i = 0; i < nodeAtoms.length; ++i)
+		{
+			if (linkId in nodeAtoms[i].tags)
+			{
+				var tagTypes = nodeAtoms[i].tags[linkId];
+				for (var j = tagTypes.length; j >= 0; --j)
+				{
+					if (tagTypes[j] === typeNodeId)
+					{
+						tagTypes.splice(j, 1);
+					}
+				}
+			}
+		}
+	};
+
+	this.clear = clearTagTypes;
+	this.add = addTagType;
+	this.remove = removeTagType;
 }
 
 //////////////////////
 // Frame Conception //
 //////////////////////
 
-function Frame(initNodes)
+function Frame(initAtoms)
 {
-	this.nodes = initNodes || [];
+	this.atoms = initAtoms || [];
 
 	/* Description:
-	// * nodes - 1d array of nodes described in 'function Node'.
-	///////////////////////////////////////////////////////////*/
+	// * atoms - 1d array of node atoms described in 'function Atom'.
+	////////////////////////////////////////////////////////////////*/
 }
 
 //////////////////////////
@@ -83,57 +304,104 @@ function FrameAnalizer(initFrame)
 {
 	var frame = initFrame;
 
-	var iterate = function(processNode)
+	var iterateAtoms = function(processNode)
 	{
-		var nodesClone = frame.nodes.slice(0);
-		for (var i = 0; i < nodesClone.length; ++i)
+		var atomsClone = frame.atoms.slice(0);
+		for (var i = 0; i < atomsClone.length; ++i)
 		{
-			processNode(nodesClone[i]);
+			processNode(atomsClone[i]);
 		}
 	};
 
-	var stringify = function(compact)
+	var stringifyFrame = function(compact)
 	{
 		return JSON.stringify(frame, null, compact ? null : '\t');
 	};
 
-	this.iterateNodes = iterate;
-	this.stringifyNodes = stringify;
+	this.iterate = iterateAtoms;
+	this.stringify = stringifyFrame;
 
 	/* Description:
 	//
 	// Analizer provides read-only access to entire frame.
 	// It allows to analize whole structure of the frame and make decision.
-	// * iterateNodes - makes copy of current frame and iterates thru it with
+	// * iterate - makes copy of current atoms and iterates thru them with
 	// given function - to analize structure from outside.
-	// * stringifyNodes - returns JSON representation of the frame structure.
-	*/
+	// * stringify - returns JSON representation of the frame structure.
+	////////////////////////////////////////////////////////////////////////*/
 }
 
 function FrameContextControl(initFrame)
 {
 	var frame = initFrame;
 
-	var getNodesById = function (nodeId)
+	var addNodeAtom = function(newAtom)
 	{
-		var result = [];
+		frame.atoms.push(newAtom);
+	};
 
-		for (var i = 0; i < frame.nodes.length; ++i)
+	var removeNodeAtom = function(nodeId, atomIndex)
+	{
+		var currentIndex = 0;
+		for (var i = 0; i < frame.atoms.length; ++i)
 		{
-			if (frame.nodes[i].id === nodeId)
+			if (frame.atoms[i].id === nodeId)
 			{
-				result.push(frame.nodes[i]);
+				if (atomIndex === currentIndex++)
+				{
+					frame.atoms.splice(i, 1);
+					return true;
+				}
 			}
 		}
-		return result.length > 0 ? new NodeControl(result) : null;
+		return false;
 	};
 
-	var addNewNode = function (newNode)
+	var swapNodeAtomPair = function (nodeId, atom01NodeIndex, atom02NodeIndex)
 	{
-		frame.nodes.push(newNode);
+		if (atom01FrameIndex === atom02FrameIndex)
+			return true;
+		var atom01FrameIndex = -1;
+		var atom02FrameIndex = -1;
+		var currentIndex = 0;
+
+		for (var i = 0; i < frame.atoms.length; ++i)
+		{
+			if (frame.atoms[i].id === nodeId)
+			{
+				if (atom01NodeIndex === currentIndex)
+				{
+					atom01FrameIndex = i;
+				}
+				if (atom02NodeIndex === currentIndex)
+				{
+					atom02FrameIndex = i;
+				}
+				++currentIndex;
+				if (atom01FrameIndex > -1 && atom02FrameIndex > -1)
+				{
+					var bufferNode = frame.atoms[atom02FrameIndex];
+					frame.atoms[atom02FrameIndex] = frame.atoms[atom01FrameIndex];
+					frame.atoms[atom01FrameIndex] = bufferNode;
+				}
+			}
+		}
 	};
 
-	var removeNodesById = function (nodeId)
+	var getNodeById = function(nodeId)
+	{
+		var nodeAtoms = [];
+		for (var i = 0; i < frame.atoms.length; ++i)
+		{
+			if (frame.atoms[i].id === nodeId)
+			{
+				nodeAtoms.push(frame.atoms[i]);
+			}
+		}
+		return nodeAtoms.length > 0 ? new NodeControl(nodeAtoms) : null;
+	};
+
+	var removeNodeById = function (nodeId)
 	{
 		for (var i = frame.nodes.length; i >= 0; --i)
 		{
@@ -144,43 +412,11 @@ function FrameContextControl(initFrame)
 		}
 	};
 
-	var swapTwoNodes = function (node01, node02)
-	{
-		if (node01 === node02)
-			return true;
-		if (
-			node01 < 0 || node02 < 0 ||
-			node01 >= frame.nodes.length || node02 >= frame.nodes.length
-		)
-			return false;
-
-		var index01 = -1;
-		var index02 = -1;
-		for (var i = frame.nodes.length; i >= 0; --i)
-		{
-			if (frame.nodes[i] === node01)
-			{
-				index01 = i;
-			}
-			if (frame.nodes[i] === node02)
-			{
-				index02 = i;
-			}
-			if (index01 >= 0 && index02 >= 0)
-			{
-				var bufferNode = frame.nodes[index02];
-				frame.nodes[index02] = frame.nodes[index01];
-				frame.nodes[index01] = bufferNode;
-				return true;
-			}
-		}
-		return false;
-	};
-
-	this.getNodeControls = getNodesById;
-	this.addNode = addNewNode;
-	this.removeNode = removeNodesById;
-	this.swapNodePair = swapTwoNodes;
+	this.addAtom = addNodeAtom;
+	this.removeAtom = removeNodeAtom;
+	this.swapAtomPair = swapNodeAtomPair;
+	this.getNodeControl = getNodeById;
+	this.removeNode = removeNodeById;
 
 	/* Description:
 	//
@@ -212,59 +448,3 @@ function FrameContextControl(initFrame)
 	// structure) make union and separation for any nodes.
 	////////////////////////////////////////////////////////////////////////////*/
 }
-
-function NodeControl(initNodeSequence)
-{
-	var nodeSequence = initNodeSequence;
-
-	// TODO: figure out how to work with node sequence content.
-	// WARNING: Current implementation is a stub!
-
-	var getNodesContent = function()
-	{
-		// STUB implementation
-		nodeSequence[0].content;
-	};
-
-	var setNodesContent = function(newContent)
-	{
-		// STUB implementation
-		for (var i = 0; i < nodeSequence.length; ++i)
-		{
-			nodeSequence[i].content = newContent;
-		}
-	};
-
-	var getNodesTagsControl = function()
-	{
-		// TODO: figure out how to control tags of several nodes via single controller.
-		;
-	};
-
-	this.getTagsControl = getNodesTagsControl;
-	this.getContent = getNodesContent;
-	this.setContent = setNodesContent;
-
-	/* Description: // TODO: write it.
-	//
-	// * getTagsControl - ...
-	// * getContent - ...
-	// * setContent - ...
-	*/
-}
-
-// TODO: design and implement
-/*
-function TagsControl(initTags)
-{
-	var tags = initTags;
-
-	this.clearTags = ;
-	this.setLink = ;
-	this.removeLink = ;
-	this.containsLink = ;
-
-	this.getLinkType = ;
-	this.setLinkType = ;
-}
-*/
